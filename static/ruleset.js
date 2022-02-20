341e011dcb2596d3014759ef4a2b6eb3850eb0a3
Alexander <ancorwin@gmail.com>
2022-02-19 23:40:25 -0800

    a little bit of frontend never hurt anybody

---
+++ static/ruleset.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('nmg-comparison').addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log(e);
+
+    });
+
+});
