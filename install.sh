a9075c6665ad96710569e2491ae91f1365c3ad5b
Cloud User <ec2-user@ip-172-30-0-65.ec2.internal>
2022-03-16 07:35:18 +0000

    Installing stuff better

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
-SERVICE_NAME="ruleset_randomizer"
-SERVICE_PATH="/lib/systemd/system/ruleset_randomizer.service"
-cp "target/debug/$SERVICE_NAME" "/usr/bin/$SERVICE_NAME"
-if test -f "$SERVICE_PATH";
-then
-  :
-else
-  cp $SERVICE_NAME.service $SERVICE_PATH
-fi
-
+SERVICE_NAME="ruleset-randomizer"
+SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
+cp $SERVICE_NAME.service $SERVICE_PATH
+cp "etc/nginx/conf.d/$SERVICE_NAME.conf" "/etc/nginx/conf.d/$SERVICE_NAME.conf"
