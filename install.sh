9ec34695b8dfb3e3cebb977e918c3441779e8be3
Cloud User <ec2-user@ip-172-30-0-65.ec2.internal>
2022-03-16 08:34:29 +0000

    better installing

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
+set -e
 SERVICE_NAME="ruleset-randomizer"
 SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
-cp $SERVICE_NAME.service $SERVICE_PATH
-cp "etc/nginx/conf.d/$SERVICE_NAME.conf" "/etc/nginx/conf.d/$SERVICE_NAME.conf"
+cargo build
+npx tailwindcss -i ./build_templates/index.css -o ./static/index.css
+sudo cp $SERVICE_NAME.service $SERVICE_PATH
+sudo cp "etc/nginx/conf.d/$SERVICE_NAME.conf" "/etc/nginx/conf.d/$SERVICE_NAME.conf"
